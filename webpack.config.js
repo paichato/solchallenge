eb226841c5194e7bcbf4b991f69d4dc4a7d44a35
paichato <tsenane14@gmail.com>
2022-04-04 05:07:25 +0200

    added babel and webpack++++

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./guess.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve("./build"),
   },
   module: {
     rules: [
